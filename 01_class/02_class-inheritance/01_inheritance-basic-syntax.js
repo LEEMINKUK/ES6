2f9461c5d8bde1bbba20c1c365faa52b9f3511fa
LEEMINKUK <cjcjcj963@gmail.com>
2024-03-08 12:56:42 +0900

    commit

--- 01_class/02_class-inheritance/01_inheritance-basic-syntax.js
+++ 01_class/02_class-inheritance/01_inheritance-basic-syntax.js
@@ -2,33 +2,33 @@
 // 클래스 상속을 사용하면 클래스를 다른 클래스로 확장할 수 있다.
 
 // 01. 상속 기본 문법
-class Animal{
+class Animal {
 
-    constructor(name, weight){
+    constructor(name, weight) {
         this.name = name;
-        this.weight =weight;
+        this.weight = weight;
     }
 
-    eat(foodWeight){
-        this.weight+= foodWeight;
+    eat(foodWeight) {
+        this.weight += foodWeight;
         console.log(`${this.name}(은)는 ${foodWeight}kg의 식사를 하고 ${this.weight}kg 이 되었습니다.`);
     }
 
-    move(loseWeight){
-        if(this.weight > loseWeight)
+    move(loseWeight) {
+        if (this.weight > loseWeight){
             this.weight -= loseWeight;
-            console.log(`${this.name}(은)는 움직임으로 인해 ${loseWeight}kg 감량되어 ${this.weight}kg 이 되었습니다.`);
-        
+        console.log(`${this.name}(은)는 움직임으로 인해 ${loseWeight}kg 감량되어 ${this.weight}kg 이 되었습니다.`);
     }
 }
+}
 
-let animal = new Animal("동물",30);
+let animal = new Animal("동물", 30);
 animal.eat(1);
 animal.move(0.5);
 
 class Human extends Animal {
 
-    develop(language){
+    develop(language) {
         console.log(`${this.name}(은)는 ${language}로 개발을 합니다.`);
     }
 }
--- 01_class/02_class-inheritance/02_method-overriding.js
+++ 01_class/02_class-inheritance/02_method-overriding.js
@@ -2,23 +2,23 @@
 // 부모 메서드 전체를 교체하지 않고, 부모 메서드를 토대로 일부 기능만 변경하고 싶을 때,
 // 부모 메서드의 기능을 확장하고 싶을 때 메서드 오버라이딩을 사용한다.
 
-class Animal{
+class Animal {
 
-    constructor(name, weight){
+    constructor(name, weight) {
         this.name = name;
-        this.weight =weight;
+        this.weight = weight;
     }
 
-    eat(foodWeight){
-        this.weight+= foodWeight;
+    eat(foodWeight) {
+        this.weight += foodWeight;
         console.log(`${this.name}(은)는 ${foodWeight}kg의 식사를 하고 ${this.weight}kg 이 되었습니다.`);
     }
 
-    move(loseWeight){
-        if(this.weight > loseWeight)
+    move(loseWeight) {
+        if (this.weight > loseWeight)
             this.weight -= loseWeight;
-            console.log(`${this.name}(은)는 움직임으로 인해 ${loseWeight}kg 감량되어 ${this.weight}kg 이 되었습니다.`);
-        
+        console.log(`${this.name}(은)는 움직임으로 인해 ${loseWeight}kg 감량되어 ${this.weight}kg 이 되었습니다.`);
+
     }
 }
 
@@ -28,7 +28,7 @@ class Tiger extends Animal {
         console.log(`${this.name}(은)는 ${target}을 공격 합니다.`);
     }
 
-    move(target){
+    move(target) {
         // super. 을 통해 부모 클래스의 메서드를 참조한다.
         super.move(0.1);
         this.attack(target);
